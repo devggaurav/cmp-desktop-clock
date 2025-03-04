75d2cea280af8752bf354e8a09decb3f0bb06458
gc <dev.gauravchauhan@gmail.com>
2025-03-04 09:25:14 +0530

    adding analog clock

--- composeApp/src/commonMain/kotlin/com/gc/desktopclock/App.kt
+++ composeApp/src/commonMain/kotlin/com/gc/desktopclock/App.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -69,16 +71,18 @@ fun App() {
             }
         }
 
-        Box(
+        Column(
             modifier = Modifier.fillMaxSize().background(MatteBlack),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
 
-            ) {
+        ) {
 
             Row(
                 horizontalArrangement = Arrangement.spacedBy(5.dp),
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.align(Alignment.TopCenter)
-            ) {
+
+                ) {
 
                 Box(
                     modifier = Modifier.wrapContentSize()
@@ -132,9 +136,15 @@ fun App() {
                     )
                 }
             }
+            Spacer(Modifier.padding(10.dp))
 
-
-            AnalogClock()
+            Box(
+                modifier = Modifier.wrapContentSize()
+                    .background(GrayDark3, shape = RoundedCornerShape(20.dp)).padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                AnalogClock()
+            }
 
 
         }
