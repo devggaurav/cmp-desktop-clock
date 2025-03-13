99229b2ff4cb371f05b7c95d7f7d7ca59121c444
gc <dev.gauravchauhan@gmail.com>
2025-03-13 10:25:22 +0530

    adding clock full screen

--- composeApp/src/commonMain/kotlin/com/gc/desktopclock/presentation/ClockFullScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gc/desktopclock/presentation/ClockFullScreen.kt
@@ -1,14 +1,19 @@
 package com.gc.desktopclock.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.gc.desktopclock.app.Route
 import com.gc.desktopclock.ui.theme.MatteBlack
+import org.koin.compose.viewmodel.koinViewModel
 
 
 //
@@ -17,12 +22,28 @@ import com.gc.desktopclock.ui.theme.MatteBlack
 //
 
 @Composable
-fun ClockFullScreenView() {
+fun ClockFullScreenView(viewModel: MainViewModel = koinViewModel()) {
+
+     val showAnalogClock by viewModel.analogClock.collectAsStateWithLifecycle()
 
     Column(
         modifier = Modifier.background(MatteBlack).fillMaxSize().statusBarsPadding()
             .navigationBarsPadding()
     ) {
+
+        Box(modifier = Modifier.fillMaxWidth().weight(1f)) {
+
+            if (showAnalogClock){
+
+
+            }else {
+
+            }
+
+
+
+        }
+
 
     }
 
---
+++ composeApp/src/commonMain/kotlin/com/gc/desktopclock/presentation/common/AdaptiveLayout.kt
@@ -0,0 +1,20 @@
+package com.gc.desktopclock.presentation.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+//
+// Created by Code For Android on 13/03/25.
+// Copyright (c) 2025 CFA. All rights reserved.
+//
+
