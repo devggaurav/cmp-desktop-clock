304a6e02986736a5be4e4561f775f9c4461ebb73
gc <dev.gauravchauhan@gmail.com>
2025-03-14 12:00:19 +0530

    adding calender view

--- composeApp/src/commonMain/kotlin/com/gc/desktopclock/presentation/ClockFullScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gc/desktopclock/presentation/ClockFullScreen.kt
@@ -12,17 +12,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.gc.desktopclock.app.Route
 import com.gc.desktopclock.presentation.common.AnalogClock
 import com.gc.desktopclock.presentation.common.DigitalClockView
+import com.gc.desktopclock.presentation.common.ItimFontFamily
 import com.gc.desktopclock.ui.theme.GrayDark3
 import com.gc.desktopclock.ui.theme.MatteBlack
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import org.koin.compose.viewmodel.koinViewModel
 
 
@@ -38,6 +47,9 @@ fun ClockFullScreenView(viewModel: MainViewModel = koinViewModel()) {
     val mins by viewModel.mins.collectAsStateWithLifecycle()
     val hours by viewModel.hours.collectAsStateWithLifecycle()
     val showAnalogClock by viewModel.analogClock.collectAsStateWithLifecycle()
+
+    val now: Instant = Clock.System.now()
+    val today: LocalDate = now.toLocalDateTime(TimeZone.currentSystemDefault()).date
 
     Column(
         modifier = Modifier.background(MatteBlack).fillMaxSize().statusBarsPadding()
@@ -75,7 +87,13 @@ fun ClockFullScreenView(viewModel: MainViewModel = koinViewModel()) {
             contentAlignment = Alignment.Center
         ) {
 
+            Text(
+                text = "${today.dayOfMonth} ${today.month} ${today.year}",
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.White,
+                fontFamily = ItimFontFamily()
 
+            )
 
         }
 
